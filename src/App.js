da4f7c2b631d25e8bb08f4981528970f5c184524
anhdoursa <anh.do@ursa.vn>
2022-07-11 17:23:07 +0700

    update

--- src/App.js
+++ src/App.js
@@ -1,9 +1,35 @@
-import { Canvas } from 'react-three-fiber';
+import { useRef } from 'react';
+import { Canvas, useFrame, extend, useThree } from 'react-three-fiber';
+import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls';
+extend({ OrbitControls });
+const Orbit = () => {
+  const { camera, gl } = useThree();
+  return <orbitControls args={[camera, gl.domElement]} />;
+};
+const Box = (props) => {
+  const ref = useRef();
+  useFrame((state) => {
+    ref.current.rotation.x += 0.01;
+    ref.current.rotation.y += 0.01;
+    ref.current.rotation.z += 0.01;
+  });
+
+  return (
+    <mesh ref={ref} {...props}>
+      <boxBufferGeometry />
+      <meshBasicMaterial color="hotpink" />
+    </mesh>
+  );
+};
 
 function App() {
   return (
-    <div>
-      <Canvas>abc</Canvas>
+    <div style={{ width: '100vw', height: '100vh' }}>
+      <Canvas camera={{ position: [3, 5, 3] }}>
+        <Orbit />
+        <axesHelper args={[5]} />
+        <Box position={[1, 1, 1]} />
+      </Canvas>
     </div>
   );
 }
